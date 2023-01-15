7ebf8809d8455464e3bef9055e847c7bbc5bc216
Mykola <107625914+kolya-kyb@users.noreply.github.com>
2023-01-15 17:36:31 +0200

    dz 1

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -31,3 +31,14 @@ const TransactionHistory = ({items}) => {
 }
 
 export default TransactionHistory;
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+      PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type:PropTypes.string.isRequired,
+      amount:PropTypes.string.isRequired,
+      currency:PropTypes.string.isRequired,
+  })),
+  
+  }
--- src/components/TransactionHistory/TransactionHistory.styled.jsx
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -8,45 +8,27 @@ export const Container = styled.div`
 `;
 
 export const Table =styled.table`
-  	width: 100%;
-	border: none;
-	margin-bottom: 20px;  
+ width: 100%;
+	margin-bottom: 20px;
+	border: 1px solid #dddddd;
+	border-collapse: collapse;
 `;
 
 export const TitleTable = styled.thead`
    th {
-    font-weight: bold;
-	text-align: left;
-	border: none;
-	padding: 10px 15px;
-	background: #d8d8d8;
-	font-size: 14px;
+	font-weight: bold;
+	padding: 5px;
+	background: #efefef;
+	border: 1px solid #dddddd
    } 
-   th:first-child {
-	border-radius: 8px 0 0 8px;
-}
-th:last-child {
-	border-radius: 0 8px 8px 0;
-}
+
 `;
 
 export const DataTable = styled.tbody`
    td {
-	text-align: left;
-	border: none;
-	padding: 10px 15px;
-	font-size: 14px;
-	vertical-align: top;
+	border: 1px solid #dddddd;
+	padding: 5px;
 }
 
-tr:nth-child(even){
-	background: #f3f3f3;
-}
 
-td:first-child {
-	border-radius: 8px 0 0 8px;
-}
-td:last-child {
-	border-radius: 0 8px 8px 0;
-}
 `;
